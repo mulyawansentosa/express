5ac41d21a8efc53a5a143c93047946985f523eaa
Development FlazHost <development@flazhost.com>
2023-01-11 20:28:50 +0700

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req,res){
-    res.send("Hello Mulyawan Sentosa dari Rangkasbitung");
+    res.send("Hello Mulyawan Sentosa dari Rangkasbitung Lebak Banten");
 });
 
 app.listen(80);
