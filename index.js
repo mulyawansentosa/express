1cf9f7e1a9818972d0bc8346aa88dde550eb937c
Development FlazHost <development@flazhost.com>
2023-01-11 22:48:09 +0700

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req,res){
-    res.send("Hello Mulyawan Sentosa dari Rangkasbitung Lebak Banten Running on Kubernetes");
+    res.send("Hello Mulyawan Sentosa dari Rangkasbitung Lebak Banten Running on Kubernetes.");
 });
 
 app.listen(80);
