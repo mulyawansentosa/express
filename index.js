ba7adbf13bd476695bf403577fd19a0fe7d2a84c
Mulyawan Sentosa <development@flazhost.com>
2023-03-27 21:36:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req,res){
-    res.send("Hello Mulyawan Sentosa dari Rangkasbitung Lebak Banten Running on Kubernetes.<br />Canggih Banget, <br />Hi Selina");
+    res.send("Hello, this CICD is working from App Runner AWS");
 });
 
 app.listen(80);
